df783309503df248f90ed621bcb12da110ea337b
Harsh <harshjat12345671@gmail.com>
2024-10-19 00:17:53 +0530

    _

--- index.js
+++ index.js
@@ -7,7 +7,10 @@ import dotenv from "dotenv";
 import sharp from "sharp";
 import path from "path";
 import fs from "fs";
+import { fileURLToPath } from "url";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const uploadsDir = path.join(__dirname, 'uploads');
 
 
